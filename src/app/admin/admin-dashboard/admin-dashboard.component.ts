7562d6f797d9f8052570a8a2b98dfeec3b9f5d9b
Matthias Gutsch <matthias.gutsch@gmail.com>
2021-07-05 07:23:58 +0200

    ok

--- src/app/admin/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin/admin-dashboard/admin-dashboard.component.ts
@@ -44,4 +44,5 @@ export class AdminDashboardComponent implements OnInit {
    
   }
 
+  
 }
--- src/app/admin/admin/admin.component.ts
+++ src/app/admin/admin/admin.component.ts
@@ -6,9 +6,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    
+  }
 
   ngOnInit() {
+    
   }
 
 }
