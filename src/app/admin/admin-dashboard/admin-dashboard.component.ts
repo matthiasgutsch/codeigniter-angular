51d856f7a889e3ef0bf1f256b7f6738242cb1a08
Matthias Gutsch <matthias.gutsch@gmail.com>
2021-07-15 23:48:59 +0200

    ok

--- src/app/admin/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin/admin-dashboard/admin-dashboard.component.ts
@@ -5,6 +5,11 @@ import interactionPlugin from '@fullcalendar/interaction';
 import {FullCalendar} from 'primeng/fullcalendar';
 import { BlogService } from '../../services/blog.service';
 import { Blog } from '../../models/blog';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TYPE_LIST } from '../constants/constants';
+import { CategoryService } from 'src/app/services/categories.service';
+import { Category } from 'src/app/models/category';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -17,32 +22,107 @@ export class AdminDashboardComponent implements OnInit {
   events: any;
   blogs: Blog;
   error: string;
+  blogForm: FormGroup;
+  typeList: any;
+  cities: Blog[];
+  format1: string = "";
+  format2: string = "";
+  selectedCity: Blog;
+  uploadError: string;
+  selectedCategories: Category;
+  selectedDate: Date;
+  date: Date;
+  categories: Category;
 
-
-  constructor(private blogService: BlogService) {
+  constructor(private blogService: BlogService,     
+    private categoryService: CategoryService,
+    private fb: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute
+    ) {
+      this.typeList = TYPE_LIST;
 
     this.events = this.blogs;
 
    }
 
   ngOnInit() {
-
     this.blogService.getBlogs().subscribe(
       (data: Blog) => this.blogs = data,
       error => this.error = error
     );
 
+    this.categoryService.getAllList().subscribe(
+      (data: Category) => this.categories = data,
+      error => this.error = error
+    );
+
+
 
     this.options = {
-      plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+      plugins:[ dayGridPlugin, timeGridPlugin, interactionPlugin ],
       header: {
-        left: 'title',
-        right: 'prev,next',
+          left: 'prev,next',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay'
       },
-      editable: false
+      editable: true
   };
-   
+
+
+
+  this.blogForm = this.fb.group({
+    id: [''],
+    title: ['', Validators.required],
+    description: ['', Validators.required],
+    is_featured: ['0'],
+    category_id: ['', Validators.required],
+    is_active: ['0'],
+    image: [''],
+    date: ['']
+  });
+
+} 
+onSubmit() {
+  const formData = new FormData();
+  formData.append('title', this.blogForm.get('title').value);
+  formData.append('description', this.blogForm.get('description').value);
+  formData.append('is_featured', this.blogForm.get('is_featured').value);
+  formData.append('category_id', this.blogForm.get('category_id').value);
+  formData.append('is_active', this.blogForm.get('is_active').value);
+  formData.append('image', this.blogForm.get('image').value);
+  formData.append('date', this.blogForm.get('date').value);
+
+  const id = this.blogForm.get('id').value;
+
+  if (id) {
+    this.blogService.updateBlog(formData, +id).subscribe(
+      res => {
+        if (res.status == 'error') {
+          this.uploadError = res.message;
+        } else {
+          this.router.navigate(['/admin/blogs']);
+        }
+      },
+      error => this.error = error
+    );
+  } else {
+    this.blogService.createBlog(formData).subscribe(
+      res => {
+        if (res.status === 'error') {
+          this.uploadError = res.message;
+        } else {
+          this.blogForm.reset();
+          this.blogService.getBlogs().subscribe(
+            (data: Blog) => this.blogs = data,
+            error => this.error = error
+          );
+        }
+      },
+      error => this.error = error
+    );
   }
 
+}
   
 }
