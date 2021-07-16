b0901879cf2fecfc5b8d27d43e702572d62c8f84
Matthias Gutsch <matthias.gutsch@gmail.com>
2021-07-16 19:33:54 +0200

    ok

--- src/app/admin/admin/admin.component.ts
+++ src/app/admin/admin/admin.component.ts
@@ -6,12 +6,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
+  currentUser: any[] ;
+  submitted = false;
+  returnUrl: string;
+  error: {};
+  loginError: string;
+  username: string;
+  password: string;
+
   constructor() { 
-    
   }
 
   ngOnInit() {
-    
-  }
+
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser') || '[]');
+    console.log(this.currentUser);
+    }
+
 
 }
