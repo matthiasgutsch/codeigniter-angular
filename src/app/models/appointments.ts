75b33e98f3e9c8f4f14c958d83049907cbd6c2ee
Matthias Gutsch <matthias.gutsch@gmail.com>
2021-07-17 16:20:14 +0200

    ok

---
+++ src/app/models/appointments.ts
@@ -0,0 +1,13 @@
+export class Appointments {
+    id: number;
+    title: string;
+    user_id: number;
+    description: string;
+    image: string;
+    category_id: string;
+    is_featured: boolean;
+    is_active: boolean;
+    created_at: Date;
+    date: string;
+
+}
---
+++ src/app/services/appointments.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { Blog } from '../models/blog';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppointmentsService {
+  serverUrl = environment.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getBlogs() {
+    return this.http.get<Blog>(this.serverUrl + 'blog/adminBlogs').pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  find(id: number) {
+    return this.http.get<Blog>(this.serverUrl + 'blog/adminBlog/' + id).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  getBlog(id: number) {
+    return this.http.get<Blog>(this.serverUrl + 'blog/adminBlog/' + id).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  createBlog(blog) {
+    return this.http.post<any>(this.serverUrl + 'blog/createBlog', blog).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  updateBlog(blog, id: number) {
+    return this.http.post<any>(this.serverUrl + 'blog/updateBlog/' + id, blog).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  deleteBlog(id: number) {
+    return this.http.delete(this.serverUrl + 'blog/deleteBlog/' + id).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(`Backend returned code ${error.status}, ` + `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError('Something bad happened. Please try again later.');
+  }
+}
