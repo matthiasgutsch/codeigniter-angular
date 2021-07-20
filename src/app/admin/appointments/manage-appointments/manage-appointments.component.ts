4a6ba506432c7f11688d368e995b98b92607ccff
Matthias Gutsch <matthias.gutsch@gmail.com>
2021-07-20 07:38:59 +0200

    ok

--- src/app/admin/appointments/manage-appointments/manage-appointments.component.ts
+++ src/app/admin/appointments/manage-appointments/manage-appointments.component.ts
@@ -13,7 +13,6 @@ import { ClientsService } from 'src/app/services/clients.service';
   templateUrl: './manage-appointments.component.html'
 })
 export class ManageAppointmentsComponent implements OnInit {
-  title = 'Appuntamenti';
   blogs: Blog;
   blog: Blog;
   categories: any = [];
