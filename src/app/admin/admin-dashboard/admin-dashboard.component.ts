be0e9b02f4d866adf1790d80555f574d887bf4b7
Matthias Gutsch <matthias.gutsch@gmail.com>
2021-08-10 18:08:16 +0200

    ok

--- src/app/admin/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin/admin-dashboard/admin-dashboard.component.ts
@@ -9,7 +9,7 @@ import { TYPE_LIST } from '../constants/constants';
 import { CategoryService } from 'src/app/services/categories.service';
 import { Category } from 'src/app/models/category';
 import { Router, ActivatedRoute } from '@angular/router';
-import {MessageService} from 'primeng/api';
+import { MessageService } from 'primeng/api';
 import { ClientsService } from 'src/app/services/clients.service';
 import { Clients } from 'src/app/models/clients';
 import { ComuniService } from 'src/app/services/comuni.service';
@@ -41,50 +41,50 @@ export class AdminDashboardComponent implements OnInit {
   date: Date;
   categories: any = [];
   category: Category;
-  productDialog:boolean = false;
+  productDialog: boolean = false;
   appointmentsDialog: boolean = false;
   clients: any = [];
   client: Clients;
   comuni: any = [];
   displayEvent: any;
 
-trackByFn(index, item) {
-  return item.id;
-}
+  trackByFn(index, item) {
+    return item.id;
+  }
 
-@ViewChild(CalendarComponent) ucCalendar: CalendarComponent;
+  @ViewChild(CalendarComponent) ucCalendar: CalendarComponent;
 
-  constructor(private blogService: BlogService,     
+  constructor(private blogService: BlogService,
     private clientsService: ClientsService,
     private appointmentsService: AppointmentsService,
     private fb: FormBuilder,
     private comuniService: ComuniService,
-    private categoryService: CategoryService, 
+    private categoryService: CategoryService,
     private router: Router,
     private messageService: MessageService,
     private route: ActivatedRoute
-    ) {
-      this.typeList = TYPE_LIST;
+  ) {
+    this.typeList = TYPE_LIST;
 
     this.events = this.appointments;
 
-   }
+  }
 
   ngOnInit() {
-    
-    
+
+
     this.appointmentsService.getAllList().subscribe(data => {
       this.calendarOptions = {
         editable: true,
         eventLimit: false,
-        
+
         header: {
           right: 'prev,next',
           left: 'title',
-          
+
         },
         events: data,
-         locale: 'it',
+        locale: 'it',
         timezone: 'UTC',
         selectable: true,
       };
@@ -101,114 +101,72 @@ trackByFn(index, item) {
       error => this.error = error
     );
 
-    
+
     this.categoryService.getAllList().subscribe(
       (data: Category) => this.categories = data,
       error => this.error = error
     );
 
 
-  this.blogForm = this.fb.group({
-    id: [''],
-    title: ['', Validators.required],
-    description: ['', Validators.required],
-    is_featured: ['0'],
-    category_id: ['', Validators.required],
-    is_active: ['0'],
-    image: [''],
-    date: ['', Validators.required]
-  });
+    this.blogForm = this.fb.group({
+      id: [''],
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      is_featured: ['0'],
+      category_id: ['', Validators.required],
+      is_active: ['0'],
+      image: [''],
+      date: ['', Validators.required]
+    });
 
-} 
-
-
-getCategoryItem(category_id: string, id: string) {
-  return this.clients.find(item => item.id === category_id);
-}
-
-
-getComuniItem(province: string, id: string) {
-  return this.comuni.find(item => item.id === province);
-}
-
-
-
-
-
-showDialog() {
-  this.productDialog = true;
-}
-
-clickButton(model: any) {
-  this.displayEvent = model;
-  
-}
-eventClick(model: any) {
-  model = {
-    event: {
-      id: model.event.id,
-      start: model.event.start,
-      title: model.event.title,
-      allDay: model.event.allDay,
-      description: model.event.description,
-      category_id: model.event.category_id
-
-      // other params
-    },
-    duration: {}
-  }
-  this.displayEvent = model;
-  this.productDialog = true;
-
-}
-
-dayClick(event) {
-  console.log('dayClick', event);
-}
-
-
-onSubmit() {
-  const formData = new FormData();
-  formData.append('title', this.blogForm.get('title').value);
-  formData.append('description', this.blogForm.get('description').value);
-  formData.append('is_featured', this.blogForm.get('is_featured').value);
-  formData.append('category_id', this.blogForm.get('category_id').value);
-  formData.append('is_active', this.blogForm.get('is_active').value);
-  formData.append('image', this.blogForm.get('image').value);
-  formData.append('date', this.blogForm.get('date').value);
-
-  const id = this.blogForm.get('id').value;
-
-  if (id) {
-    this.blogService.updateBlog(formData, +id).subscribe(
-      res => {
-        if (res.status == 'error') {
-          this.uploadError = res.message;
-        } else {
-          this.router.navigate(['/admin/blogs']);
-        }
-      },
-      error => this.error = error
-    );
-  } else {
-    this.blogService.createBlog(formData).subscribe(
-      res => {
-        if (res.status === 'error') {
-          this.uploadError = res.message;
-        } else {
-          this.blogForm.reset();
-          this.productDialog = false;
-          this.messageService.add({key: 'myKey1', severity:'success', summary: 'Congratulazione', detail: 'Aggiunto con successo'});
-          this.blogService.getBlogs().subscribe(
-            (data: Appointments) => this.appointments = data,
-            error => this.error = error
-          );
-        }
-      },
-      error => this.error = error
-    );
   }
 
-}
-  
+
+  getCategoryItem(category_id: string, id: string) {
+    return this.clients.find(item => item.id === category_id);
+  }
+
+
+  getComuniItem(province: string, id: string) {
+    return this.comuni.find(item => item.id === province);
+  }
+
+
+
+
+
+  showDialog() {
+    this.productDialog = true;
+  }
+
+  clickButton(model: any) {
+    this.displayEvent = model;
+
+  }
+  eventClick(model: any) {
+    model = {
+      event: {
+        id: model.event.id,
+        start: model.event.start,
+        title: model.event.title,
+        allDay: model.event.allDay,
+        description: model.event.description,
+        category_id: model.event.category_id
+
+        // other params
+      },
+      duration: {}
+    }
+    this.displayEvent = model;
+    this.productDialog = true;
+
+  }
+
+  dayClick(event) {
+    console.log('dayClick', event);
+  }
+
+
+
+
 }
