2c987f698eb519084722f38e62c9c863612a34d5
Matthias Gutsch <matthias.gutsch@gmail.com>
2021-11-03 07:40:40 +0100

    ok

--- src/app/admin/supports/supports-form/supports-form.component.ts
+++ src/app/admin/supports/supports-form/supports-form.component.ts
@@ -26,6 +26,9 @@ import { Billings } from 'src/app/models/billings';
 import { Contact } from 'src/app/models/contact';
 import { CmspageService } from 'src/app/cmspage/cmspage.service';
 import { Title } from '@angular/platform-browser';
+import { SupportsService } from 'src/app/services/supports.service';
+import { Supports } from 'src/app/models/supports';
+import { Observable } from "rxjs";
 
 @Component({
   selector: "app-supports-form",
@@ -33,24 +36,49 @@ import { Title } from '@angular/platform-browser';
 })
 export class SupportsFormComponent implements OnInit {
 
-  title = 'Contact Us';
+  title = 'Support';
   model = new Contact();
   submitted = false;
   error = {};
   success: any;
+  id: number;
 
-  
+  support: Supports;
+  supports: Observable<Supports[]>;
+  supportsList: any;
+
   constructor(
     private router: Router,
     private cmspageService: CmspageService,
     private titleService: Title,
+    protected route: ActivatedRoute,
+    private supportsService: SupportsService,
     private _location: Location,
   ) { }
 
   ngOnInit() {
     this.titleService.setTitle(this.title);
+
+    this.route.paramMap.subscribe((params) => {
+      const id = this.route.snapshot.paramMap.get("id");
+      this.supportsService.getId(+id)
+        .subscribe(data => {
+          console.log(data)
+          this.support = data;
+        }, error => console.log(error));
+      });
+
+      const id = this.route.snapshot.paramMap.get("id");
+
+      this.supportsService.find_billing_client(+id).subscribe(
+        (data: Billings) => (this.supportsList = data),
+        (error) => (this.error = error)
+      );
+
   }
 
+
+  
   onSubmit() {
     this.submitted = true;
     return this.cmspageService.contactForm(this.model).subscribe(
