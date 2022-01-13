432b0083e92cbb16e6f870cd903d208df59011d2
Matthias Gutsch <matthias.gutsch@gmail.com>
2022-01-13 16:13:50 +0100

    ok

--- src/app/admin/orders-wordpress/wordpress-orders-form/wordpress-orders-form.component.ts
+++ src/app/admin/orders-wordpress/wordpress-orders-form/wordpress-orders-form.component.ts
@@ -36,6 +36,169 @@ import { ISkill } from 'src/app/models/products';
 export class WordpressOrdersFormComponent implements OnInit {
   @ViewChild("myInput", { static: false }) myInputVariable: ElementRef;
 
+
+  order: any = [
+    {
+      "id": 727,
+      "parent_id": 0,
+      "number": "727",
+      "order_key": "wc_order_58d2d042d1d",
+      "created_via": "rest-api",
+      "version": "3.0.0",
+      "status": "processing",
+      "currency": "USD",
+      "date_created": "2017-03-22T16:28:02",
+      "date_created_gmt": "2017-03-22T19:28:02",
+      "date_modified": "2017-03-22T16:28:08",
+      "date_modified_gmt": "2017-03-22T19:28:08",
+      "discount_total": "0.00",
+      "discount_tax": "0.00",
+      "shipping_total": "10.00",
+      "shipping_tax": "0.00",
+      "cart_tax": "1.35",
+      "total": "29.35",
+      "total_tax": "1.35",
+      "prices_include_tax": false,
+      "customer_id": 0,
+      "customer_ip_address": "",
+      "customer_user_agent": "",
+      "customer_note": "",
+      "billing": {
+        "first_name": "John",
+        "last_name": "Doe",
+        "company": "",
+        "address_1": "969 Market",
+        "address_2": "",
+        "city": "San Francisco",
+        "state": "CA",
+        "postcode": "94103",
+        "country": "US",
+        "email": "john.doe@example.com",
+        "phone": "(555) 555-5555"
+      },
+      "shipping": {
+        "first_name": "John",
+        "last_name": "Doe",
+        "company": "",
+        "address_1": "969 Market",
+        "address_2": "",
+        "city": "San Francisco",
+        "state": "CA",
+        "postcode": "94103",
+        "country": "US"
+      },
+      "payment_method": "bacs",
+      "payment_method_title": "Direct Bank Transfer",
+      "transaction_id": "",
+      "date_paid": "2017-03-22T16:28:08",
+      "date_paid_gmt": "2017-03-22T19:28:08",
+      "date_completed": null,
+      "date_completed_gmt": null,
+      "cart_hash": "",
+      "meta_data": [
+        {
+          "id": 13106,
+          "key": "_download_permissions_granted",
+          "value": "yes"
+        }
+      ],
+      "line_items": [
+        {
+          "id": 315,
+          "name": "Woo Single #1",
+          "product_id": 93,
+          "variation_id": 0,
+          "quantity": 2,
+          "tax_class": "",
+          "subtotal": "6.00",
+          "subtotal_tax": "0.45",
+          "total": "6.00",
+          "total_tax": "0.45",
+          "taxes": [
+            {
+              "id": 75,
+              "total": "0.45",
+              "subtotal": "0.45"
+            }
+          ],
+          "meta_data": [],
+          "sku": "",
+          "price": 3
+        },
+        {
+          "id": 316,
+          "name": "Ship Your Idea &ndash; Color: Black, Size: M Test",
+          "product_id": 22,
+          "variation_id": 23,
+          "quantity": 1,
+          "tax_class": "",
+          "subtotal": "12.00",
+          "subtotal_tax": "0.90",
+          "total": "12.00",
+          "total_tax": "0.90",
+          "taxes": [
+            {
+              "id": 75,
+              "total": "0.9",
+              "subtotal": "0.9"
+            }
+          ],
+          "meta_data": [
+            {
+              "id": 2095,
+              "key": "pa_color",
+              "value": "black"
+            },
+            {
+              "id": 2096,
+              "key": "size",
+              "value": "M Test"
+            }
+          ],
+          "sku": "Bar3",
+          "price": 12
+        }
+      ],
+      "tax_lines": [
+        {
+          "id": 318,
+          "rate_code": "US-CA-STATE TAX",
+          "rate_id": 75,
+          "label": "State Tax",
+          "compound": false,
+          "tax_total": "1.35",
+          "shipping_tax_total": "0.00",
+          "meta_data": []
+        }
+      ],
+      "shipping_lines": [
+        {
+          "id": 317,
+          "method_title": "Flat Rate",
+          "method_id": "flat_rate",
+          "total": "10.00",
+          "total_tax": "0.00",
+          "taxes": [],
+          "meta_data": []
+        }
+      ],
+      "fee_lines": [],
+      "coupon_lines": [],
+      "refunds": [],
+      "_links": {
+        "self": [
+          {
+            "href": "https://example.com/wp-json/wc/v3/orders/727"
+          }
+        ],
+        "collection": [
+          {
+            "href": "https://example.com/wp-json/wc/v3/orders"
+          }
+        ]
+      }
+    }
+  ];
   pageTitle: string;
   error: string;
   uploadError: string;
@@ -313,12 +476,12 @@ export class WordpressOrdersFormComponent implements OnInit {
 
     this.ordersService.skills(+id).subscribe(
       (res)=>{
-        this.skillsValues = res;
+        this.skillsValues = this.order.line_items;
 
         this.skillsValues.forEach((e)=>{
           formArray.push(this.fb.group({
-            description: e.description,
-            qty: e.qty,
+            id: e.id,
+            name: e.name,
             price: e.price,
             itemTotal: e.qty * e.price,
           }))
@@ -363,7 +526,7 @@ export class WordpressOrdersFormComponent implements OnInit {
       }
     }
     this.subTotal = total;
-    this.vat = this.subTotal / 100 * this.fiscaltype;
+    this.vat = this.subTotal / 100 * this.company.fiscaltype;
     this.grandTotal = this.subTotal + this.vat;
 
   }
@@ -387,7 +550,7 @@ export class WordpressOrdersFormComponent implements OnInit {
   removeQuantity(i: number): void {
     let totalCostOfItem = this.blogForm.get('skills')?.value[i].qty * this.blogForm.get('skills')?.value[i].price;
     this.subTotal = this.subTotal - totalCostOfItem;
-    this.vat = this.subTotal / 100 * this.fiscaltype;
+    this.vat = this.subTotal / 100 * this.company.fiscaltype;
     this.grandTotal = this.subTotal + this.vat;
     (<FormArray>this.blogForm.get('skills')).removeAt(i);
   }
