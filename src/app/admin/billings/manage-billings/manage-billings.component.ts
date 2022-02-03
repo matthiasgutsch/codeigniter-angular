ca95bf828b6bcf18a78f58b131b2902312d9e3d0
Matthias Gutsch <matthias.gutsch@gmail.com>
2022-02-03 07:25:23 +0100

    ok

--- src/app/admin/billings/manage-billings/manage-billings.component.ts
+++ src/app/admin/billings/manage-billings/manage-billings.component.ts
@@ -72,6 +72,7 @@ export class ManageBillingsComponent implements OnInit {
     this.currentUser = JSON.parse(localStorage.getItem('currentUser') || '[]');
     const userId = this.currentUser.user_id;
 
+    this.spinner.show();
 
     this.billingsService.getAllListbyUser().subscribe(data => {
       this.billings = data;
@@ -87,7 +88,7 @@ export class ManageBillingsComponent implements OnInit {
       }));
       this.getComuni();
       this.getClients();
-
+      this.spinner.hide();
     });
 
    
